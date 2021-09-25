5aa386e19eee04c560625faf5ae17942ad74ed4f
Amjad Khan <amjadiqbalkhanniazi@gmail.com>
2021-09-25 16:01:56 +0500

    Change header according to Auth Context

--- components/Header.js
+++ components/Header.js
@@ -1,38 +1,80 @@
-import { PlusCircleOutlined } from '@ant-design/icons';
-import Search from '@/components/Search';
-import Link from 'next/link';
+import { PlusCircleOutlined,LogoutOutlined  } from "@ant-design/icons";
+import Search from "@/components/Search";
+import AuthContext from "@/context/AuthContext";
+import Link from "next/link";
+import { useContext } from "react";
 const Header = () => {
-    return ( 
+  const {user,logout}=useContext(AuthContext);
+  return (
     <header>
       <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/ ">DjEvents</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <a className="navbar-brand" href="/ ">
+            DjEvents
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarCollapse">
             <ul className="navbar-nav me-auto mb-2 mb-md-0">
               <li className="nav-item">
-                <Link  aria-current="page" href="/"><a className="nav-link active">Home</a></Link>
+                <Link aria-current="page" href="/">
+                  <a className="nav-link active">Home</a>
+                </Link>
               </li>
               <li className="nav-item">
-                <Link  href="/about"><a className="nav-link">About</a></Link>
+                <Link href="/about">
+                  <a className="nav-link">About</a>
+                </Link>
               </li>
               <li className="nav-item">
-              <Link  href="/events"><a className="nav-link">Events</a></Link>
+                <Link href="/events">
+                  <a className="nav-link">Events</a>
+                </Link>
               </li>
-              <li className="nav-item">
-              <Link  href="/events/add"><a className="nav-link">Add Events</a></Link>
-              </li>
-              <li className="nav-item">
-               <Link  href="/account/register"><a className="nav-link"><PlusCircleOutlined  style={{ verticalAlign: '-0.027em', color: 'white' }}/>&nbsp;&nbsp;Register</a></Link>
-              </li>
+              {user ? (
+                <>
+              {/* after logged in */}
+                  <li className="nav-item">
+                    <Link href="/events/add">
+                      <a className="nav-link">Add Events</a>
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link href="/events/add">
+                      <a className="nav-link"><LogoutOutlined />&nbsp;&nbsp;Logout</a>
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <>
+                 {/* after logged out */}
+                  <li className="nav-item">
+                    <Link href="/account/register">
+                      <a className="nav-link">
+                        <PlusCircleOutlined
+                          style={{ verticalAlign: "-0.027em", color: "white" }}
+                        />
+                        &nbsp;&nbsp;Login
+                      </a>
+                    </Link>
+                  </li>
+                </>
+              )}
             </ul>
-           <Search />
+            <Search />
           </div>
         </div>
       </nav>
     </header>
-     );
-}
+  );
+};
 export default Header;
